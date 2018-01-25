f61e38386de3163fff8c393f0f762e4e8125a7e9
Atul Kumar Singh <atulsingh0401@gmail.com>
2018-01-25 20:02:28 +0530

    Update insta_data_scrap.class.php

--- insta_data_scrap.class.php
+++ insta_data_scrap.class.php
@@ -6,7 +6,8 @@ Telegram - https://t.me/developeratul
 
 @license     Code and contributions have 'MIT License'
              More details: LICENSE
-
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of 
+	the Software with proper credit to original author.
 */
 class InstaData{
 	
