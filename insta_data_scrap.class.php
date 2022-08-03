223cb71561698bd6bb83336ebf7b0d933a121cca
Atul Singh <atulsingh0401@gmail.com>
2022-08-03 17:04:07 +0200

    Update insta_data_scrap.class.php

--- insta_data_scrap.class.php
+++ insta_data_scrap.class.php
@@ -11,24 +11,24 @@ Telegram - https://t.me/developeratul
 
 */
 class InstaData{
-	
+
 	public function getData($username){
-		$options  = array('http' => array('user_agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.78'
+		$options  = array('http' => array('user_agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
 						)
 					);
-					
+
 		$context  = stream_context_create($options);
 		error_reporting(~E_WARNING);
 		if(($instaLink = file_get_contents('https://www.instagram.com/' . $username, false, $context)) == false){
 			echo "Error: Link not found, user-id is invalid";
 			exit;
 		}
-		
+
 		return $instaLink;
 	}
-	
+
 	public function fetchUserDetails($username){
-		
+
 		$instaLink = $this->getData($username);
 		$instaIDPattern = '/window._sharedData = (.*)/';
 		if (!preg_match($instaIDPattern, $instaLink, $matches)) {
@@ -39,23 +39,23 @@ class InstaData{
 		$json_output = $json_output['entry_data']['ProfilePage']['0']['graphql']['user'];
 		return $json_output;
 	}
-	
+
 	public function fetchAccountDetails($username){
-		
+
 		$details = "";
 		$instaLink = $this->getData($username);
 		$detailsPattern = '/meta content=(.\d+)(.*)/';
 		if (preg_match($detailsPattern, $instaLink, $res)) {
-			if (strpos($res, 'Followers') !== false) {
+			if (strpos($res[0], 'Followers') !== false) {
 				$details = $res[1]. "" .$res[2];
 			}
 		}
 		$input_line = substr($details, 1, -23);
 		$userDetails = preg_split("/, /", $input_line);
-		
+
 		return $userDetails;
 	}
-	
+
 	public function getTimeLine($username){
 		$instaLink = $this->getData($username);
 		$instaIDPattern = '/window._sharedData = (.*)/';
@@ -68,6 +68,7 @@ class InstaData{
 		$count = count($json_output);
 		$timeLine = Array();
 		for($i=0;$i<$count;$i++){
+			error_reporting(~E_NOTICE);
 			$post_txt = $json_output[$i]['node']['edge_media_to_caption']['edges'] ? $json_output[$i]['node']['edge_media_to_caption']['edges']['0']['node']['text'] : "";
 			$post_img = $json_output[$i]['node']['display_url'];
 			$post_likes = $json_output[$i]['node']['edge_liked_by']['count'];
@@ -82,9 +83,9 @@ class InstaData{
 		}
 		return Array('data'=>$timeLine,'count'=>$count);
 	}
-	
+
 	public function getUserDetails($username){
-		
+
 		$json_output = $this->fetchUserDetails($username);
 		$userData = array();
 		$userData['img'] = $json_output['profile_pic_url_hd'];
@@ -97,48 +98,79 @@ class InstaData{
 			$userData['is_verified'] = "false";
 		$userData['id'] = $json_output['id'];
 		$userData['instaUrl'] = "https://instagram.com/".$json_output['username'];
-		$json_userData = json_encode($userData);
-		
-		return $json_userData;
+		// $json_userData = json_encode($userData);
+
+		// return $json_userData;
+		return $userData;
 	}
-	
+
 	public function getAccountDetails($username){
-		
+
 		$userDetails = $this->fetchAccountDetails($username);
 		$accountData = array();
 		$accountData['followers'] = $userDetails[0];
 		$accountData['follow'] = $userDetails[1];
 		$temp = preg_split("/ -/", $userDetails[2]);
 		$accountData['posts'] = $temp[0];
-		$json_accountData = json_encode($accountData);
-		
-		return $json_accountData;
+		// $json_accountData = json_encode($accountData);
+
+		// return $json_accountData;
+		return $accountData;
 	}
-	
-	public function getHashTagData($hashtag){
-		$options  = array('http' => array('user_agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.78'
-						)
-					);
-					
-		$context  = stream_context_create($options);
-		error_reporting(~E_WARNING);
-		if(($hashtagData = file_get_contents('https://www.instagram.com/explore/tags/' . $hashtag . '/?__a=1', false, $context)) == false){
-			echo "Error: Link not found";
+
+
+// New code added here
+
+	public function getHashTageData($hashtag){
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, 'https://i.instagram.com/api/v1/tags/logged_out_web_info/?tag_name=' . $hashtag);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+
+		curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
+
+		$headers = array();
+		$headers[] = 'Authority: i.instagram.com';
+		$headers[] = 'Accept: */*';
+		$headers[] = 'Accept-Language: en-US,en;q=0.9';
+		$headers[] = 'Cookie: csrftoken=AtXwOU4HakeAhVPX5ymMG5NerRCINGvg; mid=YuqMTwAEAAFwCGMwsQpx3HuH-L1n; ig_did=2A6AF10E-74DE-4398-A1E6-5AD079D49602; dpr=2; datr=Y4zqYg2zy9EU5ozONcNPvGYL';
+		$headers[] = 'Origin: https://www.instagram.com';
+		$headers[] = 'Referer: https://www.instagram.com/';
+		$headers[] = 'Sec-Ch-Ua: \".Not/A)Brand\";v=\"99\", \"Google Chrome\";v=\"103\", \"Chromium\";v=\"103\"';
+		$headers[] = 'Sec-Ch-Ua-Mobile: ?0';
+		$headers[] = 'Sec-Ch-Ua-Platform: \"Linux\"';
+		$headers[] = 'Sec-Fetch-Dest: empty';
+		$headers[] = 'Sec-Fetch-Mode: cors';
+		$headers[] = 'Sec-Fetch-Site: same-site';
+		$headers[] = 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36';
+		$headers[] = 'X-Asbd-Id: 198387';
+		$headers[] = 'X-Csrftoken: AtXwOU4HakeAhVPX5ymMG5NerRCINGvg';
+		$headers[] = 'X-Ig-App-Id: 936619743392459';
+		$headers[] = 'X-Ig-Www-Claim: 0';
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+		$hashtagData = curl_exec($ch);
+		if (curl_errno($ch)) {
+			echo "Error: Hash tag value is not available for " . $hashtag;
+			echo 'Error:' . curl_error($ch);
 			exit;
 		}
-		
+		curl_close($ch);
+
 		return $hashtagData;
 	}
 
 	public function getTagLikes($hashtag){
-		$instaHashtag = $this->getHashTagData($hashtag);
+		$instaHashtag = $this->getHashTageData($hashtag);
 		$json_output = json_decode($instaHashtag,true);
 		$likes = $json_output['graphql']['hashtag']['edge_hashtag_to_media']['count'];
 		return $likes;
 	}
 
 	public function getTagData($hashtag){
-		$instaHashtag = $this->getHashTagData($hashtag);
+		$instaHashtag = $this->getHashTageData($hashtag);
+		echo $instaHashtag;
 		$json_output = json_decode($instaHashtag,true);
 		$json_output = $json_output['graphql']['hashtag']['edge_hashtag_to_media']['edges'];
 		$count = count($json_output);
@@ -155,4 +187,5 @@ class InstaData{
 		}
 		return Array('data'=>$hashtag_data,'count'=>$count);
 	}
+
 }
